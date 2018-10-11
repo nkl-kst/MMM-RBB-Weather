886926403acb21f3250ef5adce9b77733d90ebee
Nikolai <louis6708@gmail.com>
2018-10-11 20:47:42 +0200

    Improved logging

--- Logger.js
+++ Logger.js
@@ -1,7 +1,34 @@
+/* Magic Mirror
+ * Module: MMM-RBB-Weather
+ *
+ * By Nikolai Keist (github.com/nkl-kst)
+ * MIT Licensed.
+ */
+
 module.exports = {
-    debug: console.debug,
-    log: console.log,
-    info: console.info,
-    warn: console.warn,
-    error: console.error,
+
+    _printLog(level, message) {
+        let now = new Date();
+        console[level](`MMM-RBB-Weather[${now.toLocaleString()}]: ${message}`);
+    },
+
+    debug: function(message) {
+        this._printLog('debug', message);
+    },
+
+    log: function(message) {
+        this._printLog('log', message);
+    },
+
+    info: function(message) {
+        this._printLog('info', message);
+    },
+
+    warn: function(message) {
+        this._printLog('warn', message);
+    },
+
+    error: function(message) {
+        this._printLog('error', message);
+    },
 };
