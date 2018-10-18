1500342c47c98c8c5be4a0bd870d6aad30eb7474
Nikolai <louis6708@gmail.com>
2018-10-18 21:18:07 +0200

    Applied ESLint rules to test JS code

--- test/unit/node_helper_Test.js
+++ test/unit/node_helper_Test.js
@@ -4,7 +4,7 @@ const proxyquire = require('proxyquire').noCallThru();
 const assert = require('assert');
 const sinon = require('sinon');
 
-const node_helper_fake = {
+const helperFake = {
     create: function(helperDefinition) {
         return helperDefinition;
     }
@@ -13,14 +13,14 @@ const node_helper_fake = {
 const LoggerFake = {
     log: function() {},
     info: function() {},
-    warn: function() {},
+    warn: function() {}
 };
 
 const HttpsFake = {
-    get: function() {},
+    get: function() {}
 };
 
-describe("node_helper", () => {
+describe('node_helper', () => {
 
     // Tested
     let helper;
@@ -29,29 +29,29 @@ describe("node_helper", () => {
         decache('../../node_helper');
         helper = proxyquire(
             '../../node_helper',
-            { 'node_helper': node_helper_fake, './Logger': LoggerFake, 'https': HttpsFake });
+            { 'node_helper': helperFake, './Logger': LoggerFake, 'https': HttpsFake });
     });
 
     afterEach(() => {
         sinon.restore();
     });
 
-    describe("socketNotificationReceived", () => {
+    describe('socketNotificationReceived', () => {
 
-        it("should set config and load data when notification is received", () => {
+        it('should set config and load data when notification is received', () => {
 
             // Arrange
             helper.loadData = sinon.fake();
 
             // Act
-            helper.socketNotificationReceived('LOAD_DATA', { test: "data" });
+            helper.socketNotificationReceived('LOAD_DATA', { test: 'data' });
 
             // Assert
-            assert.deepEqual(helper.config, { test: "data" });
+            assert.deepStrictEqual(helper.config, { test: 'data' });
             assert.ok(helper.loadData.calledOnce);
         });
 
-        it("should restrict maximum days to seven", () => {
+        it('should restrict maximum days to seven', () => {
 
             // Arrange
             helper.loadData = sinon.fake();
@@ -60,10 +60,10 @@ describe("node_helper", () => {
             helper.socketNotificationReceived('LOAD_DATA', { days: 8 });
 
             // Assert
-            assert.deepEqual(helper.config, { days: 7 });
+            assert.deepStrictEqual(helper.config, { days: 7 });
         });
 
-        it("should do nothing if notification is unknown", () => {
+        it('should do nothing if notification is unknown', () => {
 
             // Arrange
             helper.loadData = sinon.fake();
@@ -72,20 +72,20 @@ describe("node_helper", () => {
             helper.socketNotificationReceived('UNKNOWN_NOTIFICATION');
 
             // Assert
-            assert.equal(helper.config, null);
+            assert.strictEqual(helper.config, null);
             assert.ok(helper.loadData.notCalled);
         });
     });
 
-    describe("loadData", () => {
+    describe('loadData', () => {
 
-        it("should send socket notification with fetched data", async () => {
+        it('should send socket notification with fetched data', async () => {
 
             // Arrange
             helper.config = { days: 0 };
 
             let resolvedPromise = new Promise((resolve) => {
-                resolve({ test: "data" });
+                resolve({ test: 'data' });
             });
             helper.fetchDayData = sinon.fake.returns(resolvedPromise);
             helper.sendSocketNotification = sinon.fake();
@@ -94,31 +94,31 @@ describe("node_helper", () => {
             await helper.loadData();
 
             // Assert
-            assert.ok(helper.sendSocketNotification.calledWith('DATA_LOADED', [{ test: "data" }]));
+            assert.ok(helper.sendSocketNotification.calledWith('DATA_LOADED', [{ test: 'data' }]));
         });
 
-        it("should send socket notification with cached data when error occures", async () => {
+        it('should send socket notification with cached data when error occures', async () => {
 
             // Arrange
             helper.config = { days: 0 };
-            helper.cache = "cached";
+            helper.cache = 'cached';
 
-            helper.fetchDayData = sinon.fake.throws("test");
+            helper.fetchDayData = sinon.fake.throws('test');
             helper.sendSocketNotification = sinon.fake();
 
             // Act
             await helper.loadData({ days: 0 });
 
             // Assert
-            assert.ok(helper.sendSocketNotification.calledWith('DATA_LOADED', "cached"));
+            assert.ok(helper.sendSocketNotification.calledWith('DATA_LOADED', 'cached'));
         });
 
-        it("should do nothing when error occurs and no cache is available", async () => {
+        it('should do nothing when error occurs and no cache is available', async () => {
 
             // Arrange
             helper.config = { days: 0 };
 
-            helper.fetchDayData = sinon.fake.throws("test");
+            helper.fetchDayData = sinon.fake.throws('test');
             helper.sendSocketNotification = sinon.fake();
 
             // Act
@@ -129,9 +129,9 @@ describe("node_helper", () => {
         });
     });
 
-    describe("fetchDayData", () => {
+    describe('fetchDayData', () => {
 
-        it("should request the RBB url", () => {
+        it('should request the RBB url', () => {
 
             // Arrange
             HttpsFake.get = sinon.fake();
@@ -141,10 +141,10 @@ describe("node_helper", () => {
 
             // Assert
             assert.ok(HttpsFake.get.calledWith(
-                "https://www.rbb24.de/include/wetter/data/data_bb_0.xml"));
+                'https://www.rbb24.de/include/wetter/data/data_bb_0.xml'));
         });
 
-        it("should define listener for all events", async () => {
+        it('should define listener for all events', async () => {
 
             // Arrange
             let responseFake = {
@@ -152,7 +152,7 @@ describe("node_helper", () => {
                     if (event === 'end') {
                         callback(); // Need this call for resolving the promise
                     }
-                }),
+                })
             };
 
             HttpsFake.get = async function(url, callback) {
@@ -171,42 +171,42 @@ describe("node_helper", () => {
             assert.ok(responseFake.on.calledWith('end'));
         });
 
-        it("should chunk xml data and parse on end of response", async () => {
+        it('should chunk xml data and parse on end of response', async () => {
 
             // Arrange
             let responseFake = {
                 on: function(event, callback) {
                     if (event === 'data') {
-                        callback("chunk");
+                        callback('chunk');
                     }
                     if (event === 'end') {
                         callback();
                     }
-                },
+                }
             };
 
             HttpsFake.get = async function(url, callback) {
                 await callback(responseFake);
             };
 
-            helper.parseData = sinon.fake((xml) => { return xml + " parsed" });
+            helper.parseData = sinon.fake((xml) => { return xml + ' parsed'; });
 
             // Act
             let data = await helper.fetchDayData();
 
             // Assert
-            assert.equal(data, "chunk parsed");
+            assert.strictEqual(data, 'chunk parsed');
         });
 
-        it("should reject on error", async () => {
+        it('should reject on error', async () => {
 
             // Arrange
             let responseFake = {
                 on: function(event, callback) {
                     if (event === 'error') {
-                        callback(new Error("error message"));
+                        callback(new Error('error message'));
                     }
-                },
+                }
             };
 
             HttpsFake.get = async function(url, callback) {
@@ -216,18 +216,18 @@ describe("node_helper", () => {
             // Act / Assert
             await assert.rejects(async () => {
                 await helper.fetchDayData();
-            }, Error("error message"));
+            }, Error('error message'));
         });
 
-        it("should reject on timeout", async () => {
+        it('should reject on timeout', async () => {
 
             // Arrange
             let responseFake = {
                 on: function(event, callback) {
                     if (event === 'timeout') {
-                        callback(new Error("timeout message"));
+                        callback(new Error('timeout message'));
                     }
-                },
+                }
             };
 
             HttpsFake.get = async function(url, callback) {
@@ -237,48 +237,48 @@ describe("node_helper", () => {
             // Act / Assert
             await assert.rejects(async () => {
                 await helper.fetchDayData();
-            }, Error("timeout message"));
+            }, Error('timeout message'));
         });
     });
 
-    describe("parseData", () => {
+    describe('parseData', () => {
 
-        it("should return weather data", () => {
+        it('should return weather data', () => {
 
             // Arrange
-            helper.config = { id: "2a" };
+            helper.config = { id: '2a' };
             let xml = '<data><city id="1" test="other"/><city id="2" test="data"/></data>';
 
             // Act
             let data = helper.parseData(xml, 1);
 
             // Assert
-            assert.deepEqual(data, { id: "2", test: "data" });
+            assert.deepStrictEqual(data, { id: '2', test: 'data' });
         });
 
-        it("should return nothing if xml parsing fails", () => {
+        it('should return nothing if xml parsing fails', () => {
 
             // Arrange
-            let xml = "no valid xml";
+            let xml = 'no valid xml';
 
             // Act
             let data = helper.parseData(xml, 0);
 
             // Assert
-            assert.deepEqual(data, {});
+            assert.deepStrictEqual(data, {});
         });
 
-        it("should return nothing if no city id matches", () => {
+        it('should return nothing if no city id matches', () => {
 
             // Arrange
-            helper.config = { id: "no matching id" };
+            helper.config = { id: 'no matching id' };
             let xml = '<data><city id="1" test="other"/><city id="2" test="data"/></data>';
 
             // Act
             let data = helper.parseData(xml, 0);
 
             // Assert
-            assert.deepEqual(data, {});
+            assert.deepStrictEqual(data, {});
         });
     });
 });
